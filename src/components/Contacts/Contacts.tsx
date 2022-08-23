0e5ba21f7be39a1fd45b4506460405ea48ecab52
Shalaginov Ivan <shalaginov.dev@gmail.com>
2022-08-23 21:33:38 +0300

    contact cards changed

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -55,55 +55,53 @@ export const Contacts = (props: ContactsPropsType) => {
     }
 
     return (
-        <div>
-            <div className={s.contactWrapper}>
-                <div className={s.userPhotoWrapper}>
-                    <img alt={'ava'}
-                         src={props.photo}
-                         className={s.userPhoto}/>
+        <div className={s.contactWrapper}>
+            <div className={s.userPhotoWrapper}>
+                <img alt={'ava'}
+                     src={props.photo}
+                     className={s.userPhoto}/>
+            </div>
+            <div className={s.contactData}>
+                {
+                    validationError && <span className={s.validationErrorEdit}>max length 18 symbols</span>
+                }
+                <div>
+                    <EditableSpan name={'name'}
+                                  id={props.id}
+                                  title={props.name}
+                                  editMode={props.editMode}
+                                  changeContactHandler={changeContactHandler}
+                                  keyPressHandler={onKeyPressHandler}
+                                  onChange={() => {
+                                  }}/>
                 </div>
-                <div className={s.contactData}>
-                    {
-                        validationError && <span className={s.validationErrorEdit}>max length 18 symbols</span>
-                    }
-                    <div>
-                        <EditableSpan name={'name'}
-                                      id={props.id}
-                                      title={props.name}
-                                      editMode={props.editMode}
-                                      changeContactHandler={changeContactHandler}
-                                      keyPressHandler={onKeyPressHandler}
-                                      onChange={() => {
-                                      }}/>
-                    </div>
-                    <div>
-                        <EditableSpan name={'phone'}
-                                      id={props.id}
-                                      title={props.phone}
-                                      editMode={props.editMode}
-                                      changeContactHandler={changeContactHandler}
-                                      keyPressHandler={onKeyPressHandler}
-                                      onChange={() => {
-                                      }}/>
-                    </div>
+                <div>
+                    <EditableSpan name={'phone'}
+                                  id={props.id}
+                                  title={props.phone}
+                                  editMode={props.editMode}
+                                  changeContactHandler={changeContactHandler}
+                                  keyPressHandler={onKeyPressHandler}
+                                  onChange={() => {
+                                  }}/>
                 </div>
-                <div className={s.editDeleteWrapper}>
-                    {
-                        props.editMode ?
-                            <button className={s.confirmContactButton} onClick={() => {
-                                confirmHandler()
-                            }}>confirm</button>
-                            :
-                            <button className={s.editContactButton} onClick={() => {
-                                dispatch(ChangeEditModeAction(props.id, true))
-                            }}>edit
-                            </button>
-                    }
-                    <button className={s.deleteContactButton} onClick={() => {
-                        dispatch(DeleteContactAction(props.id))
-                    }}>delete
-                    </button>
-                </div>
+            </div>
+            <div className={s.editDeleteWrapper}>
+                {
+                    props.editMode ?
+                        <button className={s.confirmContactButton} onClick={() => {
+                            confirmHandler()
+                        }}>confirm</button>
+                        :
+                        <button className={s.editContactButton} onClick={() => {
+                            dispatch(ChangeEditModeAction(props.id, true))
+                        }}>edit
+                        </button>
+                }
+                <button className={s.deleteContactButton} onClick={() => {
+                    dispatch(DeleteContactAction(props.id))
+                }}>delete
+                </button>
             </div>
         </div>
     )
